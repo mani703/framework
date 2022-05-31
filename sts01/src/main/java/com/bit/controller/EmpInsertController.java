51dc334e181b43db8a1d41e7d8648de408ba5cff
mani703 <mani703@naver.com>
2022-05-31 17:42:17 +0900

    spring webMVC & JDBC

---
+++ sts01/src/main/java/com/bit/controller/EmpInsertController.java
@@ -0,0 +1,43 @@
+package com.bit.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.bit.model.Emp01Dao;
+import com.bit.model.Emp02Dao;
+import com.bit.model.Emp03Dao;
+import com.bit.model.EmpDao;
+import com.bit.model.EmpVo;
+
+public class EmpInsertController implements Controller {
+	EmpDao dao;
+	
+	public void setDao(EmpDao dao) {
+		this.dao = dao;
+	}
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		if("POST".equals(request.getMethod())) {
+			request.setCharacterEncoding("utf-8");
+			
+			EmpVo bean = new EmpVo(
+						Integer.parseInt(request.getParameter("empno")),
+						request.getParameter("ename"),
+						Integer.parseInt(request.getParameter("sal")),
+						request.getParameter("job")
+					);
+			dao.insertOne(bean);
+			
+			mav.setViewName("redirect:list.bit");
+		}else {
+			mav.setViewName("emp/add");
+		}
+		return mav;
+	}
+
+}
---
+++ sts01/src/test/java/com/bit/model/EmpDaoTest.java
@@ -0,0 +1,64 @@
+package com.bit.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EmpDaoTest {
+	static EmpVo target;
+	static int before;
+	static EmpDao dao;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		target = new EmpVo(1008, "test", 1000, "test");
+		ApplicationContext context = null;
+		context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		dao = (EmpDao) context.getBean("dao");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test1SelectAll() throws SQLException {
+		before = dao.selectAll().size();
+		assertTrue(before > 0);
+	}
+
+	@Test
+	public void test2InsertOne() throws SQLException {
+		dao.insertOne(target);
+		assertSame(before+1, dao.selectAll().size());
+	}
+
+	@Test
+	public void test4SelectOne() throws SQLException {
+		assertEquals(target, dao.selectOne(target.getEmpno()));
+	}
+
+	@Test
+	public void test3UpdateOne() throws SQLException {
+		target.setEname("수정");
+		assertTrue(dao.updateOne(target)>0);
+	}
+
+	@Test
+	public void test5DeleteOne() throws SQLException {
+		assertTrue(dao.deleteOne(target.getEmpno())>0);
+		assertSame(before, dao.selectAll().size());
+	}
+
+}
